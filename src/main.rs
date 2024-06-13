8a7f46ee61531e96dbc5a6e25ef454d4547673ff
Felix D <fdrees@uni-bremen.de>
2024-06-13 23:25:42 +0200

    Removed test case since code didn't pass QA

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 // Copyright (C) 2023  Felix Drees
 
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, Button, Label, Notebook, Box};
+use gtk::{Application, ApplicationWindow, Button, Label, Notebook, Box, DropDown};
 
 fn main() { // -> glib::ExitCode {
     let app = Application::builder()
@@ -16,24 +16,47 @@ fn main() { // -> glib::ExitCode {
 fn build_ui(app: &Application) {
     let app_name = option_env!("CARGO_PKG_NAME").unwrap_or("AI Lab");
     let version = option_env!("CARGO_PKG_VERSION").unwrap_or("0.0.0");
-    let title = format!("{} - by: Felix D. - v{}", app_name, version);
+    let title = format!("{} - by Felix D. - v{}", app_name, version);
 
     let window = ApplicationWindow::builder()
         .title(&title)
         .application(app)
+        .default_width(700)
+        .default_height(500)
         .build();
-        //.default_width(700)
-        //.default_height(500)
-    window.set_default_size(600, 400);
 
     let notebook = Notebook::new();
     window.set_child(Some(&notebook));
 
-    // === BEGIN Workspace sub page ================================================================
+    notebook.append_page(&workspace_ui(), Some(&Label::new(Some("Workspace"))));
+
+    let page2_label = Label::new(Some("Annotation"));
+    notebook.append_page(&page2_label, Some(&Label::new(Some("Annotation"))));
+
+    let page3_label = Label::new(Some("Preprocessing"));
+    notebook.append_page(&page3_label, Some(&Label::new(Some("Preprocessing"))));
+
+    let page4_label = Label::new(Some("Training"));
+    notebook.append_page(&page4_label, Some(&Label::new(Some("Training"))));
+
+    let page5_label = Label::new(Some("Postprocessing"));
+    notebook.append_page(&page5_label, Some(&Label::new(Some("Postprocessing"))));
+
+    let page6_label = Label::new(Some("Prediction"));
+    notebook.append_page(&page6_label, Some(&Label::new(Some("Prediction"))));
+
+    let page7_label = Label::new(Some("Evaluation"));
+    notebook.append_page(&page7_label, Some(&Label::new(Some("Evaluation"))));
+
+    window.show();  // window.present();
+}
+
+
+fn workspace_ui() -> gtk::Box {
     // container for ui elements
     // -------------------------
     let container = Box::builder()
-        .orientation(gtk::Orientation::Vertical)
+        .orientation(gtk::Orientation::Horizontal)
         .margin_top(24)
         .margin_bottom(24)
         .margin_start(24)
@@ -43,15 +66,20 @@ fn build_ui(app: &Application) {
         .spacing(24)
         .build();
 
+    let container_left = Box::builder()
+        .orientation(gtk::Orientation::Vertical)
+        .spacing(24)
+        .build();
+
     // add label to container
     // ----------------------
     let title = Label::builder()
-        .label("Example Label")
+        .label("Load existing workspace")
         .halign(gtk::Align::Start)
         .build();
 
-    title.add_css_class("title-2");
-    container.append(&title);
+    title.add_css_class("title-3");
+    container_left.append(&title);
 
     // add button to container
     // -----------------------
@@ -59,13 +87,12 @@ fn build_ui(app: &Application) {
         .label("select workspace")
         .build();
 
-    container.append(&button);
 
     // Connect button click event to show file chooser dialog
     button.connect_clicked(move |_| {
         // Create a new file chooser dialog
         let dialog = gtk::FileChooserDialog::builder()
-            .title("Select a Directory")
+            .title("Select a workspace .yaml file")
             .action(gtk::FileChooserAction::Open)
             .build();
 
@@ -87,32 +114,50 @@ fn build_ui(app: &Application) {
         // Show the dialog
         dialog.show();
     });
+    container_left.append(&button);
+    container.append(&container_left);
 
-    container.append(&gtk::Label::builder().label("or\ncreate a new workspace (todo)").build());
+    container.append(&gtk::Label::builder().label("or").build());
 
-    notebook.append_page(&container, Some(&Label::new(Some("Workspace"))));
-    // === END Workspace sub page ==================================================================
+    let container_right = Box::builder()
+        .orientation(gtk::Orientation::Vertical)
+        .spacing(24)
+        .build();
 
-    let page2_label = Label::new(Some("Annotation"));
-    notebook.append_page(&page2_label, Some(&Label::new(Some("Annotation"))));
+    let title2 = Label::builder()
+        .label("Create new workspace")
+        .halign(gtk::Align::Start)
+        .build();
 
-    let page3_label = Label::new(Some("Preprocessing"));
-    notebook.append_page(&page3_label, Some(&Label::new(Some("Preprocessing"))));
+    title2.add_css_class("title-3");
+    container_right.append(&title2);
 
-    let page4_label = Label::new(Some("Training"));
-    notebook.append_page(&page4_label, Some(&Label::new(Some("Training"))));
+    // Drop Down for selecting the problem type
+    // -----------------------------------------
+    let problem_types = vec!["Classification (Predicting Data)", "Clustering (Grouping)"];
 
-    let page5_label = Label::new(Some("Postprocessing"));
-    notebook.append_page(&page5_label, Some(&Label::new(Some("Postprocessing"))));
+    let expression = gtk::PropertyExpression::new(
+        gtk::StringObject::static_type(),
+        None::<gtk::Expression>,
+        "string",
+    );
 
-    let page6_label = Label::new(Some("Prediction"));
-    notebook.append_page(&page6_label, Some(&Label::new(Some("Prediction"))));
+    let drop_down = DropDown::new(Some(&gtk::StringList::new(problem_types.as_slice())),
+                                  Some(expression));
+    // drop_down.set_enable_search(true);
+    // drop_down.set_search_match_mode(gtk::StringFilterMatchMode::Substring);
 
-    let page7_label = Label::new(Some("Evaluation"));
-    notebook.append_page(&page7_label, Some(&Label::new(Some("Evaluation"))));
+    container_right.append(&drop_down);
 
-    // ---------------------------------------------------------------------------------------------
-    
-    window.show();
-    // window.present();
+    // TODO(felix): fi the user selected classification, add a section where the user
+    //  can select what classes to classify
+
+    // TODO(felix): add a text filed for a filename to write the workspace config to
+
+    // TODO(felix): add a selection for data directory selection
+    //  maybe also add a directory for the labels
+
+    container.append(&container_right);
+
+    container
 }
